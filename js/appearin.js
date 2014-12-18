c4d40e3168845efd3090b86bec00dee48f1be8d7
Mathias Bynens <mathias@qiwi.be>
2014-12-18 16:16:02 +0100

    Normalize directory structure

---
+++ js/appearin.js
@@ -0,0 +1,21 @@
+window.appearin = new function() {
+
+	var APPEAR_IN_URL = 'https://appear.in/';
+
+	this.linkToRoom = function(room) {
+		return APPEAR_IN_URL + room;
+	};
+
+	this.getRandomRoom = function() {
+		var postPromise = http.post(
+			'https://api.appear.in/random-room-name'
+		).then(function(xhr) {
+			var roomName = JSON.parse(xhr.responseText).roomName;
+			return roomName.substring(1, roomName.length);
+		}).catch(function() {
+			return 'random' + (Math.random() * 1000000000 | 0);
+		});
+		return postPromise;
+	};
+
+};
